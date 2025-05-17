c9d2dfb8dd34afef6bfcd2f89b357c8f7d46c9ac
Fasilurahman <131866940+Fasilurahman@users.noreply.github.com>
2025-05-17 18:34:05 +0530

    Update server.ts options

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,8 @@ const corsOptions = {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     exposedHeaders: ['Authorization'] // Required for token refresh headers
   };
-  
+
+app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
