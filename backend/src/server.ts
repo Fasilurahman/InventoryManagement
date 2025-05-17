ea7d52b11b2591692a6d7f5a9551fa4e50bd737f
Fasilurahman <131866940+Fasilurahman@users.noreply.github.com>
2025-05-17 18:23:20 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
